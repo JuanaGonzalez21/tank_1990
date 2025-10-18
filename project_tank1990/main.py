07df2dafa37437585f56450b7a906c7932326175
Juana Gonzalez <76920092+JuanaGonzalez21@users.noreply.github.com>
2025-10-18 11:36:44 -0500

    3 commit

--- project_tank1990/main.py
+++ project_tank1990/main.py
@@ -1,17 +1,24 @@
 import pygame
 import time
 import random
+import pygame.freetype
 
 WIDTH, HEIGHT = 1920, 1080
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tank 1990")
 
 BG = pygame.transform.scale(pygame.image.load("bg.jpg"), (  WIDTH, HEIGHT))
+WHITE = (255 ,255 ,255)
 
 #Montamos el background
 def draw():
     WIN.blit(BG, (0,0))
     pygame.display.update()
+
+def text_bottom(text, font_size, text_rgb, bg_rgb):
+    font = pygame.freetype.Sysfont("Courier", font_size, bold=True)
+    surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
 
 
 def main():
